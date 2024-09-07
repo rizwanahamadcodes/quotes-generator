f37c20c9cac72f68ff4e484cbca29679bf4aaeb5
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2024-09-07 20:38:39 +0545

    implemented wrapper click to close drawer

--- js/main.js
+++ js/main.js
@@ -10,5 +10,12 @@ const handleCloseBtnClick = () => {
     drawerWrapper.classList.remove("shown");
 };
 
+const handleDrawerWrapperClick = (e) => {
+    if (e.target.classList.contains("drawer-wrapper")) {
+        drawerWrapper.classList.remove("shown");
+    }
+};
+
 categoryBtn.addEventListener("click", handleCategoryBtnClick);
 closeBtn.addEventListener("click", handleCloseBtnClick);
+drawerWrapper.addEventListener("click", handleDrawerWrapperClick);
